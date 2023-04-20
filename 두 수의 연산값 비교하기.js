039f818ae3d4bc90a37b0d79807cde02ec2636b5
YunaPark <jeannelvn@gmail.com>
2023-04-20 15:04:18 +0900

    소문자로 바꾸기

--- 두 수의 연산값 비교하기.js
+++ 두 수의 연산값 비교하기.js
@@ -1,15 +1,15 @@
 (function () {
-  function solution(a, b) {
-    let str = Number(String(a) + String(b));
-    let num = 2 * a * b;
-    if (str > num) {
-      return str;
-    } else if (str < num) {
-      return num;
-    } else if (str === num) {
-      return str;
+    function solution(a, b) {
+        let str = Number(String(a) + String(b));
+        let num = 2 * a * b;
+        if (str > num) {
+            return str;
+        } else if (str < num) {
+            return num;
+        } else if (str === num) {
+            return str;
+        }
     }
-  }
 
-  console.log(solution(2, 91));
+    console.log(solution(2, 91));
 })();
