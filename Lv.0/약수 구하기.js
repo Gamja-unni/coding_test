e6567be299bc8e766de3749d3d04468ccc1c35c1
[SW5] 박유나 <jeannelvn@gmail.com>
2023-07-21 00:27:33 +0900

    add: 약수 구하기

---
+++ Lv.0/약수 구하기.js
@@ -0,0 +1,14 @@
+/*
+정수 n이 매개변수로 주어질 때, 
+n의 약수를 오름차순으로 담은 배열을 
+return하도록 solution 함수를 완성해주세요.
+*/
+
+function solution(n) {
+    const arr = [];
+    for (let i = 0; i <= n; i++) {
+        if (n % i === 0) arr.push(i);
+    }
+    return arr.sort((a, b) => a - b);
+}
+console.log(solution(24));
