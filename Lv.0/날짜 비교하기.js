e0493cd7eb4a6f2444c6ec7795df6a05d97bb5c2
[SW5] 박유나 <jeannelvn@gmail.com>
2023-09-07 16:14:13 +0900

    add: 날짜 비교하기

---
+++ Lv.0/날짜 비교하기.js
@@ -0,0 +1,24 @@
+/*
+정수 배열 date1과 date2가 주어집니다. 
+두 배열은 각각 날짜를 나타내며 [year, month, day] 꼴로 주어집니다. 
+각 배열에서 year는 연도를, month는 월을, day는 날짜를 나타냅니다.
+
+만약 date1이 date2보다 앞서는 날짜라면 1을, 아니면 0을 return 하는 solution 함수를 완성해 주세요.
+*/
+
+function solution(date1, date2) {
+    const dateStr1 = Number(date1.join(""));
+    const dateStr2 = Number(date2.join(""));
+
+    console.log(dateStr1 - dateStr2);
+
+    if (dateStr1 === dateStr2) return 0;
+    if (dateStr1 !== dateStr2) return 1;
+}
+
+// {
+//     if (dateStr1 > dateStr2) return dateStr1 - dateStr2;
+//     if (dateStr1 < dateStr2) return dateStr2 - dateStr1;
+// }
+
+console.log(solution([2021, 12, 28], [2021, 12, 29]));
