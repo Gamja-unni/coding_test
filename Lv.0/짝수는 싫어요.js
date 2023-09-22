06334921d5b6fbd95a58ae5e3e85ce95d7231148
[SW5] 박유나 <jeannelvn@gmail.com>
2023-09-22 09:54:05 +0900

    add: 짝수는 싫어요

---
+++ Lv.0/짝수는 싫어요.js
@@ -0,0 +1,13 @@
+(function () {
+    function solution(n) {
+        const answer = [];
+        for (let i = 0; i <= n; i++) {
+            if (i % 2 === 1) {
+                answer.push(i);
+            }
+        }
+        return answer;
+    }
+
+    console.log(solution(15));
+})();
